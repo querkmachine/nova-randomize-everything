a51f93b40ea969cc2bf781a546a295009341fbed
beeps (Kim Grey) <kimberly.grey@digital.cabinet-office.gov.uk>
2023-03-07 10:57:48 +0000

    Use `nova.crypto` UUID function if it's available #3

--- src/generators/uuid.js
+++ src/generators/uuid.js
@@ -1,4 +1,10 @@
 export function uuidV4() {
+  // if nova.crypto is available, use it
+  if("crypto" in nova) {
+    return nova.crypto.randomUUID().toLowerCase()
+  }
+
+  // if not, generate one manually
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
     const r = (Math.random() * 16) | 0,
       v = c == "x" ? r : (r & 0x3) | 0x8;
