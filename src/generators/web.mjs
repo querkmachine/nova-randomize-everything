6cc19c9b08f56dba11af00a14d65d2f0f791c0e2
beeps (Kim Grey) <kimberly.grey@digital.cabinet-office.gov.uk>
2023-03-10 16:37:03 +0000

    Start refactoring web generators

--- src/generators/web.mjs
+++ src/generators/web.mjs
@@ -7,81 +7,162 @@ import {
 } from "./util/utilities";
 
 const lorem = new LoremIpsum();
-
-const localPartJoiners = ["", ".", "_"];
-
+const defaultDomainExtensions = [
+  ".com",
+  ".net",
+  ".org",
+  ".info",
+  ".biz",
+  ".edu",
+];
 const realEmailDomains = [
+  "aol.com",
   "gmail.com",
   "googlemail.com",
-  "yahoo.com",
-  "outlook.com",
   "hotmail.com",
-  "protonmail.com",
-  "aol.com",
   "icloud.com",
+  "msn.com",
+  "outlook.com",
+  "protonmail.com",
+  "yahoo.com",
 ];
 
-const defaultDomainExtensions = [".com", ".net", ".org"];
-
-function stripPunctuation(str) {
-  return str.replace(/[\W_]+/g, "");
+function sanitize(str) {
+  // Replace diacritics
+  str = replaceDiacritics(str);
+  // Remove punctuation
+  str = str.replace(/[\W_]+/g, "");
+  return str;
 }
 
-function emailLocalPart() {
-  const localPart1 = stripPunctuation(
-    Math.random() > 0.5 ? lorem.generateWords(1) : nameFirst()
-  );
-  const localPart2 =
-    Math.random() > 0.25
-      ? stripPunctuation(
-          Math.random() > 0.5 ? lorem.generateWords(1) : nameLast()
+export function webUsername() {
+  const usernameJoiners = ["", ".", "-", "_", "+"];
+  const noOfParts = generateNumber(1, 4);
+  const parts = [];
+
+  // Put some parts together
+  for (let i = 0; i < noOfParts; i++) {
+    // 90% chance of word, 10% chance of digit
+    if (Math.random() < 0.9) {
+      parts.push(
+        sanitize(
+          pickFromArray([lorem.generateWords(1), nameFirst(), nameLast()])
         )
-      : "";
-  const localPart3 = Math.random() > 0.5 ? generateNumber(2, 99) : "";
-  return replaceDiacritics(
-    `${localPart1}${pickFromArray(localPartJoiners)}${localPart2}${localPart3}`
+      );
+    } else {
+      parts.push(String(generateNumber(0, 999)));
+    }
+    // Additional 25% chance of punctuation if this isn't the last iteration
+    // of the loop
+    if (i != noOfParts - 1 && Math.random() < 0.25) {
+      parts.push(pickFromArray(usernameJoiners));
+    }
+  }
+
+  // Join them together
+  return parts.join("");
+}
+
+export function webDomainName({
+  domainExtensions = defaultDomainExtensions,
+} = {}) {
+  domainExtensions = domainExtensions.length
+    ? domainExtensions
+    : defaultDomainExtensions;
+  const domainNameJoiners = ["", "-"];
+  const noOfParts = generateNumber(1, 3);
+  const parts = [];
+
+  for (let i = 0; i < noOfParts; i++) {
+    // 95% chance of word, 5% chance of digit
+    if (Math.random() < 0.95) {
+      parts.push(
+        sanitize(
+          pickFromArray([lorem.generateWords(1), nameFirst(), nameLast()])
+        )
+      );
+    } else {
+      parts.push(String(generateNumber(0, 999)));
+    }
+  }
+
+  return (
+    parts.join(pickFromArray(domainNameJoiners)) +
+    pickFromArray(domainExtensions)
   ).toLowerCase();
 }
 
-export function webDomainName({ domainExtensions = [] } = {}) {
-  const tlds = domainExtensions.length
-    ? domainExtensions
-    : defaultDomainExtensions;
-  return `${stripPunctuation(
-    lorem.generateWords(generateNumber(1, 3))
-  )}${pickFromArray(tlds)}`.toLowerCase();
-}
-
 export function webEmail({
-  domainExtensions = [],
+  domainExtensions = defaultDomainExtensions,
   useRealEmailDomains = false,
 } = {}) {
-  const tlds = domainExtensions.length
+  domainExtensions = domainExtensions.length
     ? domainExtensions
     : defaultDomainExtensions;
-  const emailDomainPart = useRealEmailDomains
+  const domainPart = useRealEmailDomains
     ? pickFromArray(realEmailDomains)
-    : webDomainName({ domainExtensions: tlds });
-  return `${emailLocalPart()}@${emailDomainPart}`.toLowerCase();
+    : webDomainName({ domainExtensions });
+  return `${webUsername().toLowerCase()}@${domainPart}`;
 }
 
-export function webURL({ domainExtensions = [] } = {}) {
-  const tlds = domainExtensions.length
+export function webURL({
+  domainExtensions = defaultDomainExtensions,
+  urlsIncludeSubdomains = true,
+  urlsIncludeFilePaths = true,
+  urlsIncludeQueryStrings = true,
+  urlsIncludeHashes = true,
+} = {}) {
+  domainExtensions = domainExtensions.length
     ? domainExtensions
     : defaultDomainExtensions;
-  let str = Math.random() > 0.5 ? "https" : "http";
-  str += "://";
-  str +=
-    Math.random() > 0.5
-      ? "www."
-      : Math.random() > 0.5
-      ? lorem.generateWords(1) + "."
-      : "";
-  str += webDomainName({ domainExtensions: tlds });
-  str += "/" + lorem.generateWords(1);
-  str += "/" + lorem.generateWords(1);
-  str += "?" + lorem.generateWords(1) + "=" + lorem.generateWords(1);
-  str += "&" + lorem.generateWords(1) + "=" + generateNumber(1, 9999);
-  str += "#" + lorem.generateWords(1);
-  return str.toLowerCase();
+  const joiners = ["", "-", "_"];
+  const parts = [];
+
+  // Protocol
+  parts.push(pickFromArray(["http://", "https://"]));
+
+  // Subdomain
+  if (urlsIncludeSubdomains) {
+    parts.push(pickFromArray(["www.", `${sanitize(lorem.generateWords(1))}.`]));
+  }
+
+  // Domain
+  parts.push(webDomainName());
+
+  // File path/extension
+  if (urlsIncludeFilePaths) {
+    const noOfFilePathParts = generateNumber(1, 3);
+    const filePathParts = [];
+    for (let i = 0; i < noOfFilePathParts; i++) {
+      let string = sanitize(lorem.generateWords(generateNumber(1, 3)));
+      string = string.replace(" ", pickFromArray(joiners));
+      filePathParts.push(pickFromArray([string, generateNumber(1, 1000)]));
+    }
+    parts.push(`/${filePathParts.join("/")}`);
+  }
+
+  // Query string
+  if (urlsIncludeQueryStrings) {
+    const noOfQueryStringParts = generateNumber(1, 3);
+    const queryStringParts = [];
+    for (let i = 0; i < noOfQueryStringParts; i++) {
+      let key = sanitize(lorem.generateWords(generateNumber(1, 3)));
+      key = key.replace(" ", pickFromArray(joiners));
+      const value = pickFromArray([
+        sanitize(lorem.generateWords(1)),
+        generateNumber(1, 1000000),
+      ]);
+      queryStringParts.push(`${key}=${value}`);
+    }
+    parts.push(`?${queryStringParts.join("&")}`);
+  }
+
+  // Hash
+  if (urlsIncludeHashes) {
+    let string = sanitize(lorem.generateWords(generateNumber(1, 3)));
+    string = string.replace(" ", pickFromArray(joiners));
+    parts.push(`#${string}`);
+  }
+
+  return parts.join("").toLowerCase();
 }
